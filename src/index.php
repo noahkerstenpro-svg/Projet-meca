33e59585734f65f02464b26e956d5b00cfc6f36e
Camara Gagny <gagnycamara@icloud.com>
2026-03-12 10:00:56 +0000

    Modifications docker-compose et index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,21 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h1><?php echo "Hello World ?"; ?></h1>
-</body>
-</html>
+<?php
+ 
+// INDEX.PHP VULNÉRABLE POUR TEST IDS
+
+// SQL Injection
+$id = $_GET['id'] ?? '';
+echo "<h2>SQL test : ID = $id</h2>";
+
+// XSS
+$name = $_GET['name'] ?? '';
+echo "<h2>XSS test : Name = $name</h2>";
+
+// Local File Inclusion (LFI)
+$page = $_GET['page'] ?? 'home';
+if (file_exists("./pages/$page.php")) {
+    include("./pages/$page.php");
+} else {
+    echo "<p>Page non trouvée</p>";
+}
+?>
 
