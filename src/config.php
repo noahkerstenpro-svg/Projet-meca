44c518c1af602a23a7e56e33882d628b134d7c26
noahkerstenpro-svg <noahkersten.pro@gmail.com>
2026-04-30 10:00:45 +0200

    Update LDAP configuration and comments

--- src/config.php
+++ src/config.php
@@ -4,12 +4,10 @@
 define('LDAP_HOST', 'ldap://192.168.11.75');
 define('LDAP_PORT', 389);
 
-// Domaine
+//Nom de domaine
 define('LDAP_DOMAIN', 'ciel.com');
 
-// Base DN (vu dans ton PowerShell)
 define('LDAP_BASE_DN', 'DC=ciel,DC=com');
-
-// ⚠️ À adapter selon tes groupes réels
-define('GROUP_ELEVES', 'CN=Eleves,OU=Users,DC=ciel,DC=com');
+//Groupes
+define('GROUP_ELEVES', 'CN=Eleves,CN=Users,DC=ciel,DC=com');
 define('GROUP_PROFS', 'CN=Profs,CN=Users,DC=ciel,DC=com');
