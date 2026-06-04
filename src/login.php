4922cfcfa86533594309e1c8ae4d3dfabc329624
noah <noahkersten.pro@gmail.com>
2026-06-04 11:43:53 +0200

    teste demo

--- src/login.php
+++ src/login.php
@@ -81,7 +81,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Connexion - Atelier mécanique</title>
+    <title>Connexion! - Atelier mécanique</title>
     <style>
         * {
             box-sizing: border-box;
